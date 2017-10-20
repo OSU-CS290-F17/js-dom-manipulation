ededf7f5c4b6ac53617e86b31196f8a52616e290
Rob Hess <robwhess@gmail.com>
2017-10-20 16:01:56 -0700

    Add code from 10/20 lecture.

---
+++ manipulate.js
@@ -0,0 +1,59 @@
+function createPhotoCard(photoURL, caption) {
+
+  var photoCardSection = document.createElement('section');
+  photoCardSection.classList.add('photo-card');
+
+  var imgContainerDiv = document.createElement('div');
+  imgContainerDiv.classList.add('img-container');
+  photoCardSection.appendChild(imgContainerDiv);
+
+  var img = document.createElement('img');
+  img.classList.add('person-photo-img');
+  img.src = photoURL;
+  imgContainerDiv.appendChild(img);
+
+  var captionDiv = document.createElement('div');
+  captionDiv.classList.add('caption');
+  captionDiv.textContent = caption;
+  photoCardSection.appendChild(captionDiv);
+
+  console.log("photoCardSection:", photoCardSection);
+
+  photoCardSection.setAttribute('data-price', '500');
+
+  return photoCardSection;
+
+}
+
+var newPhotoCard = createPhotoCard("http://placekitten.com/320/320/", "A cute kitty");
+
+var photoCardContainer = document.getElementById('photo-card-container');
+
+photoCardContainer.appendChild(newPhotoCard);
+
+
+var userSpecifiedContent = "<img src=x onerror=\"alert('Uh oh!')\">";
+// photoCardContainer.innerHTML += "<div class=\"lorem-card\">" + userSpecifiedContent + "</div>";
+
+// photoCardContainer.textContent += userSpecifiedContent;
+
+var newDiv = document.createElement('div');
+newDiv.classList.add('lorem-card');
+
+// newDiv.style.color = 'orange';
+// newDiv.style.border = '3px dashed purple';
+newDiv.classList.add('ugly');
+
+var newSpan = document.createElement('span');
+newSpan.classList.add('ugly');
+
+var newTextNode = document.createTextNode(userSpecifiedContent);
+newSpan.appendChild(newTextNode);
+
+newDiv.appendChild(newSpan);
+
+// console.log("newDiv:", newDiv);
+
+photoCardContainer.appendChild(newDiv);
+
+newSpan.textContent = "Hi";
